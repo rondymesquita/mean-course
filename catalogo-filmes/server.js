bbc69f99184605097daf1b8804ffb0c163e22c78
rondymesquita <rondinellimesquitas@gmail.com>
2015-12-24 11:37:09 -0300

    On going

--- catalogo-filmes/server.js
+++ catalogo-filmes/server.js
@@ -1,33 +1,39 @@
-var express 		= require("express");
-var path 			= require('path');
-var bodyParser     	= require('body-parser');
-var methodOverride 	= require('method-override');
-var routes			= require('./app/routes/routes.js')
-var app = express();
+//var express 		= require("express");
+var http 			= require('http');
 
-app.engine('html', require('ejs').renderFile);
-app.set('view engine','ejs');
-app.set('views', path.join(__dirname,'/app/views'));
 
-// get all data/stuff of the body (POST) parameters
-// parse application/json
-app.use(bodyParser.json());
-// parse application/vnd.api+json as json
-app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
-// override with the X-HTTP-Method-Override header in the request. simulate DELETE/PUT
-app.use(methodOverride('X-HTTP-Method-Override'));
-// set the static files location /public/img will be /img for users
-app.use(express.static(__dirname + '/public'));
 
-routes(app);
+var app 			= require('./config/express')();
+//var app = express();
 
-app.set('port', process.env.PORT || 3000);
 
-var server = app.listen(app.get('port'), function(){
-	//console.log(server);
-	console.log('Server started at '+ server.address().port);
+// app.engine('html', require('ejs').renderFile);
+// app.set('view engine','ejs');
+// app.set('views', path.join(__dirname,'/app/views'));
+//
+// // get all data/stuff of the body (POST) parameters
+// // parse application/json
+// app.use(bodyParser.json());
+// // parse application/vnd.api+json as json
+// app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+// // parse application/x-www-form-urlencoded
+// app.use(bodyParser.urlencoded({ extended: true }));
+// // override with the X-HTTP-Method-Override header in the request. simulate DELETE/PUT
+// app.use(methodOverride('X-HTTP-Method-Override'));
+// // set the static files location /public/img will be /img for users
+// app.use(express.static(__dirname + '/public'));
+//
+// routes(app);
+//
+// app.set('port', process.env.PORT || 3000);
+//
+// var server = app.listen(app.get('port'), function(){
+// 	//console.log(server);
+// 	console.log('Server started at '+ server.address().port);
+// });
+//
+// exports = module.exports = app;
+
+var server = http.createServer(app).listen(app.get('port'),function(){
+	console.log('==> Server started at '+ server.address().port);
 });
-
-exports = module.exports = app;
